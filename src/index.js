ce8722f7f5bbb8bc1624ed5df553e9d7d9bc401f
RewiKaze <lnwkazeazelass@hotmail.co.th>
2021-05-03 18:20:27 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,55 @@ import "./index.css";
 import App from './App';
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
 import { SessionProvider } from './contexts/SessionContext'
 import reportWebVitals from "./reportWebVitals";
+import {setContext} from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'https://funiture-backend.herokuapp.com/graphql',
+});
+
+const getCookie = (cname) => {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+const authLink = setContext((_, { headers }) => {
+  const token = getCookie("token")
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5001/graphql',
   cache: new InMemoryCache(),
   credentials: 'include',
+  link: authLink.concat(httpLink),
 })
+
+
+
+
+// const client = new ApolloClient({
+//   uri: 'https://funiture-backend.herokuapp.com/graphql',
+//   cache: new InMemoryCache(),
+//   credentials: 'include',
+// })
 
 ReactDOM.render(
   <React.StrictMode>
